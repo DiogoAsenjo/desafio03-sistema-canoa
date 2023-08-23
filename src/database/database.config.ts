21da9095bf28175c97559966c3b6de5c2ac119d0
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-23 10:53:40 -0300

    Terminando de configurar o banco de dados

---
+++ src/database/database.config.ts
@@ -0,0 +1,36 @@
+import { Sequelize } from 'sequelize-typescript';
+import pg from 'pg';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: process.env.HOST,
+        port: 5432, // A porta padrão do PostgreSQL
+        username: process.env.USER,
+        password: process.env.PASSWORD,
+        database: 'postgres',
+        dialectModule: pg, //Necessário para o deploy na Vercel
+        dialectOptions: { //Necessário para usar o servidor Postgre no Azure 
+            ssl: { 
+                require: true
+            }
+        }
+      });
+      sequelize.addModels([]) //Aqui irei colocar os modelos
+      await sequelize.sync();
+
+      try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+      
+     
+      return sequelize;
+    },
+  },
+];
---
+++ src/database/database.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { databaseProviders } from './database.config';
+
+@Module({
+    controllers: [],
+    providers: [...databaseProviders],
+    exports: [...databaseProviders]
+})
+export class DatabaseModule {}
