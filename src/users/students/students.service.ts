58aee2fdcdb980806cc7bfe314562362f962a1a7
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-28 14:05:06 -0300

    Criando a rota de login

--- src/users/students/students.service.ts
+++ src/users/students/students.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './student.dto';
+import { CreateStudentDto } from './create-student.dto';
 import { Student } from './student.entity';
+import { LoginDto } from './login.dto';
 
 @Injectable()
 export class StudentsService {
@@ -15,5 +16,21 @@ export class StudentsService {
         if(alreadyRegistered) return 'User already exists, you should use another email';
         await Student.create({...newUser});
         return 'Student registered sucessfully!';
+    }
+
+    async login(loginData: LoginDto): Promise<string> {
+      const userExists = await Student.findOne({
+        where: {
+          email: loginData.email,
+        },
+      });
+
+      if(!userExists) return "User doesn't exist, check your credentials";
+      
+      const validPassword = userExists.password === loginData.password;
+
+      if(!validPassword) return "Invalid password, try again!";
+
+      return "Logged in succesfully!"
     }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,12 @@
 import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
-import { Student } from './students/student.entity';
 import { Response } from 'express';
-//import { InterfaceStudent } from './students/student.entity';
-import { CreateStudentDto } from './students/student.dto';
+import { CreateStudentDto } from './students/create-student.dto';
 import { StudentsService } from './students/students.service';
-
-const users: Array<Object>= []
+import { LoginDto } from './students/login.dto';
 
 @Controller()
 export class UsersController {
-    constructor(private readonly studentService: StudentsService) {}
+    constructor(private readonly studentsService: StudentsService) {}
 
     @Get()
     mainPage(): string {
@@ -20,11 +17,23 @@ export class UsersController {
     @Post('signup')
     async createUser(@Res() res: Response, @Body() userData: CreateStudentDto): Promise<void> {
         try {
-            const response = await this.studentService.createAccount(userData);
+            const response = await this.studentsService.createAccount(userData);
             res.status(HttpStatus.OK).send(response);
         } catch(error) {
             console.log(error);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
         }
-    }    
+    }
+    
+    //LOGIN
+    @Post()
+    async login(@Res() res: Response, @Body() loginData: LoginDto): Promise<void> {
+        try {
+            const response = await this.studentsService.login(loginData);
+            res.status(HttpStatus.OK).send(response);
+        } catch(error) {
+            console.log(error);
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+        }
+    }
 }
