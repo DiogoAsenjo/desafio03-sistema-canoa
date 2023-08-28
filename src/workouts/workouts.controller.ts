f1cdd3bf64090481be42ec88c356ee0742ee1797
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-28 16:16:40 -0300

    Rota para exibição de todos os treinos

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { CreateWorkoutDto } from './create-workout.dto';
 import { Workout } from './workout.entity';
@@ -8,11 +8,33 @@ import { WorkoutsService } from './workouts.service';
 export class WorkoutsController {
     constructor(private readonly workoutsService: WorkoutsService) {}
 
+    //CREATE WORKOUT
     @Post()
     async createWorkout(@Res() res: Response, @Body() workoutData: CreateWorkoutDto): Promise<void> {
         try {
             await Workout.create({...workoutData});
             res.status(HttpStatus.OK).send("Workout created succesfully!");
+        } catch(error) {
+            console.log(error);
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
+        }
+    }
+
+    //SHOW ALL WORKOUTS
+    @Get('all')
+    async showAllWorkouts(@Res() res: Response): Promise<void> {
+        try {
+            const workouts = await Workout.findAll();
+            const allWorkouts = workouts.map((item) => {
+                return {
+                    date: item.date,
+                    timeSpent: item.timeSpent,
+                    distance: item.distance,
+                    maxSpeed: item.maxSpeed,
+                    averageSpeed: item.averageSpeed
+                }
+            })
+            res.status(HttpStatus.OK).send(allWorkouts);
         } catch(error) {
             console.log(error);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
