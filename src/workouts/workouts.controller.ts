7166df5a1b1895476f211c1215491dbf1ff0d9b5
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-01 12:56:53 -0300

    Refatorações finais nas respostas da API

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -14,8 +14,11 @@ export class WorkoutsController {
     @Post()
     async createWorkout(@Res() res: Response, @Body() workoutData: CreateWorkoutDto): Promise<void> {
         try {
-            await Workout.create({...workoutData});
-            res.status(HttpStatus.OK).send("Workout created succesfully!");
+            const newWorkout = await Workout.create({...workoutData});
+            res.status(HttpStatus.OK).send({
+                message: "Workout created succesfully!",
+                workout: newWorkout
+            });
         } catch(error) {
             console.log(error);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
@@ -27,7 +30,8 @@ export class WorkoutsController {
     async showAllWorkouts(@Res() res: Response): Promise<void> {
         try {
             const workouts = await Workout.findAll();
-            const allWorkouts = workouts.map((item) => {
+            //Não lembro pra que servia isso e porque não passava direto o workouts, vou deixar aqui por enquanto por precaução.
+            /* const allWorkouts = workouts.map((item) => {
                 return {
                     id: item.id,
                     date: item.date,
@@ -36,8 +40,8 @@ export class WorkoutsController {
                     maxSpeed: item.maxSpeed,
                     averageSpeed: item.averageSpeed
                 }
-            })
-            res.status(HttpStatus.OK).send(allWorkouts);
+            }) */
+            res.status(HttpStatus.OK).send(workouts);
         } catch(error) {
             console.log(error);
             res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error);
