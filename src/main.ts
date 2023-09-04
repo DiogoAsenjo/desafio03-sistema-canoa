18f26e73a3aff49c71196795f7c91103a4db4e70
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-04 12:40:36 -0300

    Renomeando arquivo

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
-    customSiteTitle: 'Backend Generator',
+    customSiteTitle: 'Sistema canoa',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
