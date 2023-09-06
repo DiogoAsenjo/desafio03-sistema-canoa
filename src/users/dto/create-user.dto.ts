5387a7b143072a412feb49d089628ef7636114e3
DiogoAsenjo <diogoasenjo@outlook.com>
2023-09-06 11:02:35 -0300

    Alterações finais no swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,22 +2,22 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsEmail, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ description: 'User full name' })
+  @ApiProperty({ description: 'User full name', example: 'Name Example' })
   @IsNotEmpty({ message: 'Full name field is mandatory'})
   fullName: string;
 
-  @ApiProperty({ description: 'User cellphone number' })
+  @ApiProperty({ description: 'User cellphone number', example: '(XX)9XXXX-XXXX' })
   @IsNotEmpty({ message: 'Cellphone field is mandatory'})
   cellphone: string;
 
-  @ApiProperty({ description: 'User email address' })
+  @ApiProperty({ description: 'User email address', example: 'example@email.com' })
   @IsNotEmpty({ message: 'E-mail field is mandatory'})
   @IsEmail({}, { message: 'E-mail should be valid' })
   email: string;
 
   @ApiProperty({
     description: 'User password, should have at least 8 caracters and one of each: lowercase, uppercase, a number and a symbol',
-    example: 'Abc@1234',
+    example: 'Example@2023',
   })
   @IsNotEmpty({ message: 'Password field is mandatory'})
   @IsStrongPassword({
--- src/users/dto/login.dto.ts
+++ src/users/dto/login.dto.ts
@@ -3,11 +3,11 @@ import { IsNotEmpty } from "class-validator";
 
 export class LoginDto {
 
-    @ApiProperty({ description: 'User e-mail' })
+    @ApiProperty({ description: 'User e-mail', example: 'example@email.com' })
     @IsNotEmpty({ message: 'E-mail field is mandatory'})
     email: string;
 
-    @ApiProperty({ description: 'User password' })
+    @ApiProperty({ description: 'User password', example: 'Example@2023' })
     @IsNotEmpty({ message: 'Password field is mandatory'})
     password: string;
 }
